b438f82ad91f0a8a28309d8033c24a3ddaf0d9d5
Tim Vigneau <tjvig94@gmail.com>
2021-01-21 22:41:17 -0500

    Textarea changes color based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,38 @@
 $(document).ready(function() {
 
+    let now = dayjs();
+
+    const hourRows = [
+        "nine-am", "ten-am", "eleven-am", "twelve-pm", "one-pm", "two-pm", "three-pm", "four-pm", "five-pm"
+    ]
+
+    // function to get the task associated with the time from local storage
     function displayTask(time) {
         $("#" + time).val(localStorage.getItem(time));    
     };
-
-    displayTask("nine-am");
-    displayTask("ten-am");
-    displayTask("eleven-am");
-    displayTask("twelve-pm");
-    displayTask("one-pm");
-    displayTask("two-pm");
-    displayTask("three-pm");
-    displayTask("four-pm");
-    displayTask("five-pm");
-
+    // place the task in each associated time row
+    hourRows.forEach(element => {
+        displayTask(element);
+    });
+    // saves task written in textbox to local storage
     function saveTask() {
         let currentHour = $(this).data("time");
         let currentTask = $("#" + currentHour).val();
-        let currentTaskBox = $("#" + currentHour);
         if (currentTask !== "") {
-            localStorage.setItem(currentHour, currentTask); 
-        
+            localStorage.setItem(currentHour, currentTask);        
         };
     };
-    
+
+    hourRows.forEach(element => {
+        let currentElement = $("#" + element);
+        if (currentElement.data("hour") == now.$H) {
+            currentElement.css("background-color", "green");
+        } else if (currentElement.data("hour") < now.$H) {
+            currentElement.css("background-color", "gray");
+        } else if (currentElement.data("hour") > now.$H) {
+            currentElement.css("background-color", "blue")
+        };
+    }); 
+    // Save function when the save button is clicked
     $(".btn").on("click", saveTask);
 });
